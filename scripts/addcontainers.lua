8492c257270e65b584208abf3d0c4b2197cc64d4
Lettuce <wangmowen2000@163.com>
2022-12-16 11:51:22 +0800

    fix chest slots

--- scripts/addcontainers.lua
+++ scripts/addcontainers.lua
@@ -9,46 +9,55 @@ AddClassPostConstruct("widgets/containerwidget", function (self)
     local _open = self.Open
 
     function self:Open(container, doer)
+
         _open(self, container, doer)
 
         local widget = container.replica.container:GetWidget()
-        if widget.spslotpos == nil then
-            return
-        end
+        if widget.spslotpos ~= nil then
+            for i, v in ipairs(widget.spslotpos or {}) do
+                local bgoverride = widget.slotbg ~= nil and widget.slotbg[i] or nil
+                local slot = InvSlot(i,
+                    bgoverride ~= nil and bgoverride.atlas or "images/hud.xml",
+                    bgoverride ~= nil and bgoverride.image or "inv_slot.tex",
+                    self.owner,
+                    container.replica.container)
 
-        local constructionsite = doer.components.constructionbuilderuidata ~= nil and doer.components.constructionbuilderuidata:GetContainer() == container and doer.components.constructionbuilderuidata:GetConstructionSite() or nil
-        local constructionmats = constructionsite ~= nil and constructionsite:GetIngredients() or nil
-        local inv_num = #self.inv
+                self.inv[i] = self:AddChild(slot)
+                slot:SetPosition(v)
+                slot:SetScale(scale,scale,scale)
+                slot.highlight_scale = 1.3 * 1.9
+                slot.base_scale = 1 * scale
 
-        for i, v in ipairs(widget.spslotpos or {}) do
-            local bgoverride = widget.slotbg ~= nil and widget.slotbg[inv_num + i] or nil
-            local slot = InvSlot(inv_num + i,
-                bgoverride ~= nil and bgoverride.atlas or "images/hud.xml",
-                bgoverride ~= nil and bgoverride.image or (constructionmats ~= nil and "inv_slot_construction.tex" or "inv_slot.tex"),
-                self.owner,
-                container.replica.container)
-
-            self.inv[inv_num + i] = self:AddChild(slot)
-            slot:SetPosition(v)
-            slot:SetScale(scale,scale,scale)
-            slot.highlight_scale = 1.3 * 1.9
-            slot.base_scale = 1 * scale
-
-            if not container.replica.container:IsSideWidget() then
-                if widget.top_align_tip ~= nil then
-                    slot.top_align_tip = widget.top_align_tip
-                else
-                    slot.side_align_tip = (widget.side_align_tip or 0) - v.x
+                if not container.replica.container:IsSideWidget() then
+                    if widget.top_align_tip ~= nil then
+                        slot.top_align_tip = widget.top_align_tip
+                    else
+                        slot.side_align_tip = (widget.side_align_tip or 0) - v.x
+                    end
                 end
             end
 
-            if constructionmats ~= nil then
-                slot:ConvertToConstructionSlot(constructionmats[i], constructionsite:GetSlotCount(i))
+            for i, v in ipairs(widget.spslotpos2 or {}) do
+                local bgoverride = widget.slotbg ~= nil and widget.slotbg[#widget.spslotpos + i] or nil
+                local slot = InvSlot(#widget.spslotpos + i,
+                    bgoverride ~= nil and bgoverride.atlas or "images/hud.xml",
+                    bgoverride ~= nil and bgoverride.image or "inv_slot.tex",
+                    self.owner,
+                    container.replica.container
+                )
+                self.inv[#widget.spslotpos + i] = self:AddChild(slot)
+
+                slot:SetPosition(v)
+
+                if not container.replica.container:IsSideWidget() then
+                    if widget.top_align_tip ~= nil then
+                        slot.top_align_tip = widget.top_align_tip
+                    else
+                        slot.side_align_tip = (widget.side_align_tip or 0) - v.x
+                    end
+                end
             end
-
         end
-
-        self.container = container
 
         self:Refresh()
     end
@@ -64,14 +73,12 @@ function containers.widgetsetup(container, prefab, data, ...)
             container[k] = v
         end
         if container.widget.spslotpos ~= nil then
-            local total_slots = (container.widget.slotpos ~= nil and #container.widget.slotpos or 0) + (container.widget.spslotpos ~= nil and #container.widget.spslotpos or 0)
-            container:SetNumSlots(total_slots)
-            return
+            container:SetNumSlots(#container.widget.spslotpos + (container.widget.slotpos ~= nil and #container.widget.slotpos or 0) + (container.widget.spslotpos2 ~= nil and #container.widget.spslotpos2 or 0))
+        else
+            return _widgetsetup(container, prefab, data, ...)
         end
     end
-    return _widgetsetup(container, prefab, data, ...)
 end
-------------------------------------------------------------------------
 ------------------------------------------------------------------------
 
 -- 大箱子
@@ -81,6 +88,7 @@ params.treasurechest_succulent =
     {
         slotpos = {},
         spslotpos = {GLOBAL.Vector3(80 * -0.5 - 80 * 2 + 80, 80 * 2.5 - 80 * 2 + 80, 0)},
+        spslotpos2 = {},
         animbank = "ui_succulentchest_5x5",
         animbuild = "ui_succulentchest_5x5",
         pos = GLOBAL.Vector3(0, 200, 0),
@@ -91,12 +99,12 @@ params.treasurechest_succulent =
 
 for y = 3, 2, -1 do
     for x = 1, 3 do
-        table.insert(params.treasurechest_succulent.widget.slotpos, GLOBAL.Vector3(80 * x - 80 * 2 + 80, 80 * y - 80 * 2 + 80, 0))
+        table.insert(params.treasurechest_succulent.widget.spslotpos2, GLOBAL.Vector3(80 * x - 80 * 2 + 80, 80 * y - 80 * 2 + 80, 0))
     end
 end
 for y = 1, -1, -1 do
     for x = -1, 3 do
-        table.insert(params.treasurechest_succulent.widget.slotpos, GLOBAL.Vector3(80 * x - 80 * 2 + 80, 80 * y - 80 * 2 + 80, 0))
+        table.insert(params.treasurechest_succulent.widget.spslotpos2, GLOBAL.Vector3(80 * x - 80 * 2 + 80, 80 * y - 80 * 2 + 80, 0))
     end
 end
 
@@ -155,6 +163,6 @@ end
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
 for k, v in pairs(params) do
-    local total_slots = (v.widget.slotpos ~= nil and #v.widget.slotpos or 0) + (v.widget.spslotpos ~= nil and #v.widget.spslotpos or 0)
+    local total_slots = (v.widget.slotpos ~= nil and #v.widget.slotpos or 0) + (v.widget.spslotpos ~= nil and #v.widget.spslotpos or 0) + (v.widget.spslotpos2 ~= nil and #v.widget.spslotpos2 or 0)
     containers.MAXITEMSLOTS = math.max(containers.MAXITEMSLOTS, total_slots)
 end
